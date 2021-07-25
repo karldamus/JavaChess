7ae4a474a7744c2f18f5da9eab7ee92b177edfb9
Karl Damus <getaholdofkarl@gmail.com>
2021-07-25 00:20:27 -0400

    isLegalMove updates

--- src/pieces/Pawn.java
+++ src/pieces/Pawn.java
@@ -9,6 +9,27 @@ public class Pawn extends Piece {
 
     @Override
     public boolean isLegalMove(Board board, int initialRank, int initialFile, int finalRank, int finalFile) {
+        int pawnRankDistance = Math.abs(finalRank - initialRank);
+        int pawnFileDistance = Math.abs(finalFile - initialFile);
+
+        // greater than 2 squares on the rank or 1 square on the file
+        if (pawnRankDistance > 2 || pawnFileDistance > 1)
+            return false;
+        // 2 space first move check
+        if (pawnRankDistance == 2 && this.hasMoved())
+            return false;
+        // check if diagonal 1 square move is capturing a piece
+        if (pawnRankDistance == 1 && pawnFileDistance == 1) {
+            if (board.getSpaces()[finalRank][finalFile].getPiece() == null)
+                return false;
+        }
+        // backwards movement check
+        if (this.isWhite() && (finalRank > initialRank))
+            return false;
+        if (!this.isWhite() && (finalRank < initialRank))
+            return false;
+
+        // all cases covered, therefore it is a legal move
         return true;
     }
 }
