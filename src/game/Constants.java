bed29fbc5aa6af37dca0ca479992d5e69d6753e1
Karl Damus <getaholdofkarl@gmail.com>
2021-07-27 00:09:01 -0400

    Create Constants.java
    Reduce repetitiveness throughout classes.

---
+++ src/game/Constants.java
@@ -0,0 +1,8 @@
+package game;
+
+public interface Constants {
+    static final char[] fileArray = new char[] {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'};
+    static final int[] rankArray = new int[] {8, 7, 6, 5, 4, 3, 2, 1};
+    static final int RANKS = 8;
+    static final int FILES = 8;
+}
