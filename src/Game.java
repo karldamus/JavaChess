28b6cb8031464e71351d33b187705ef61e54cddd
Karl Damus <getaholdofkarl@gmail.com>
2021-08-05 00:58:21 -0400

    Initial Swing GUI setup.

--- src/Game.java
+++ src/Game.java
@@ -1,2 +1,30 @@
-public class Game {
+import game.Board;
+import game.Constants;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+public class Game implements Constants {
+
+    private JPanel boardPanel;
+
+    public Game() {
+        Board board = new Board();
+
+        setUpGui();
+
+
+
+    }
+
+    public void setUpGui() {
+        boardPanel = new JPanel(BOARD_GRID_LAYOUT);
+        boardPanel.setSize(BOARD_WIDTH, BOARD_HEIGHT);
+        boardPanel.setMaximumSize(new Dimension(BOARD_WIDTH, BOARD_HEIGHT));
+        boardPanel.setMinimumSize(new Dimension(BOARD_WIDTH, BOARD_HEIGHT));
+    }
+
+
 }
