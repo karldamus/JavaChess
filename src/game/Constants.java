d652cfa5738121272f3ca9402e2d83fe778c975c
karldamus <getaholdofkarl@gmail.com>
2021-08-11 01:30:12 -0400

    Added GUI related constants

--- src/game/Constants.java
+++ src/game/Constants.java
@@ -4,6 +4,7 @@ import java.awt.*;
 
 public interface Constants {
     static final char[] fileArray = new char[] {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'};
+    static final String[] fileArrayStr = new String[] {"a", "b", "c", "d", "e", "f", "g", "h"};
     static final int[] rankArray = new int[] {8, 7, 6, 5, 4, 3, 2, 1};
     static final int RANKS = 8;
     static final int FILES = 8;
@@ -17,4 +18,8 @@ public interface Constants {
     static final Dimension BOARD_SIZE = new Dimension(BOARD_WIDTH, BOARD_HEIGHT);
     static final Dimension LABEL_SIZE = new Dimension(LABEL_WIDTH, LABEL_HEIGHT);
     static final GridLayout BOARD_GRID_LAYOUT = new GridLayout(RANKS, FILES);
+
+    static final String GAME_TITLE = "Java Chess by Karl";
+
+    String BASE_FEN_STRING = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w  - 0 1";
 }
