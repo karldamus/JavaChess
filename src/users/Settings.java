68e82f73bfa2a01bb9c73e91c95242583e2d1491
karldamus <getaholdofkarl@gmail.com>
2021-09-01 22:18:02 -0400

    Update Settings.java
    Added getters and setters

--- src/users/Settings.java
+++ src/users/Settings.java
@@ -2,5 +2,38 @@ package users;
 
 public class Settings {
     private boolean soundOn;
-    
+    private int difficulty;
+    private ColourScheme colourScheme;
+
+    public Settings() {
+        this.soundOn = true;
+        this.difficulty = 3000;
+        this.colourScheme = new ColourScheme(SchemePresets.DEFAULT);
+    }
+
+    // ========================
+
+    public boolean isSoundOn() {
+        return soundOn;
+    }
+
+    public void setSoundOn(boolean soundOn) {
+        this.soundOn = soundOn;
+    }
+
+    public int getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(int difficulty) {
+        this.difficulty = difficulty;
+    }
+
+    public ColourScheme getColourScheme() {
+        return colourScheme;
+    }
+
+    public void setColourScheme(ColourScheme colourScheme) {
+        this.colourScheme = colourScheme;
+    }
 }
